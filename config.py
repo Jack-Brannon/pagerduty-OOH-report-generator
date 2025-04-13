5740c4f000205ffcd163abebb1d0381c2eb4d99e
jack-brannon <166420733+Jack-Brannon@users.noreply.github.com>
2025-04-13 20:33:14 +0100

    Removing calander link and names

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 # Default calendar URLs
 DEFAULT_CALENDARS = [
-    "webcal://hmrc.pagerduty.com/private/a13654a5f44a0e831e470615154de87736fb90d207471110/feed/P3TJ56J",
-    "webcal://hmrc.pagerduty.com/private/a13654a5f44a0e831e470615154de87736fb90d207471110/feed/P474259"
+    
 ]
 
 # Default date range
@@ -22,15 +21,6 @@ BANK_HOLIDAYS =  [
 
 # Specify list of people to check
 PEOPLE = [
-    "Sunil Sethumadhavan",
-    "Jack Brannon",
-    "Haseem Isaac",
-    "Gavin Zhao",
-    "Ian Wood",
-    "Garry Peace",
-    "Alexander Flexen",
-    "Ponmani Sundarapandian",
-    "Lingamurthy Bathini",
-    "Andrew Bintcliffe"
-    # Add more names as needed
+    
+    # Add names as needed
 ]
